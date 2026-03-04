8658aecfe0f699c8722d54f1453d0569bf8a4867
Stefanus <stephanuswilfrid@gmail.com>
2026-03-04 12:37:58 +1100

    push

--- src/modules/youtube/video-id-container.tsx
+++ src/modules/youtube/video-id-container.tsx
@@ -1,11 +1,13 @@
 "use client";
 import clsx from "clsx";
 import { useSearchParams } from "next/navigation";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import ReactPlayer from "react-player/youtube";
 import { SubtitleSettings } from "./transcripts/components/react/settings";
 import { SubtitleSettingsProvider, useSubtitleSettings } from "./transcripts/provider/subtitle-settings";
 import { DictEntryProvider, useDictEntry } from "./transcripts/provider/dict-entry";
+import { SubtitleTabs } from "./transcripts/components/react/tabs";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
 
 export function VideoIdContainer() {
@@ -28,12 +30,53 @@ function TranscriptComponent() {
 
     const searchParams = useSearchParams();
 
+    const onTimestampClick = useCallback(
+      (start: number, end?: number) => {
+        setDrawerOpen((prevDrawerOpen) => {
+          if (prevDrawerOpen) {
+            if (end) {
+              const duration = end - start;
+              setPlaying(() => {
+                playerRef.current?.seekTo(start / 1000, "seconds");
+                toStopAfterRef.current = duration;
+                return true; // drawer is open, autoplay timestamp
+              });
+            }
+            return true;
+          }
+  
+          // drwaer not open, proceed as usual
+          setPlaying((prevPlaying: boolean) => {
+            const wasPlaying = prevPlaying;
+  
+            playerRef.current?.seekTo(start / 1000, "seconds");
+  
+            if (wasPlaying || wasPlaying == null) {
+              return true;
+            }
+  
+            return false;
+          });
+          return prevDrawerOpen; // drawer not open, do nothing
+        });
+      },
+      [setDrawerOpen]
+    );
+
     const lastWatched = parseInt(searchParams.get("last_watched") ?? "0");
+    const { width } = useWindowSize();
+
+    const getCurrentTime = useCallback(() => {
+      if (!playerRef.current) return null;
+      const seconds = playerRef.current.getCurrentTime();
+      return seconds;
+    }, []);
 
 
   return (
     <div>
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row md:gap-3 xl:gap-6 p-0 sm:p-0 md:p-3 xl:p-6">
+        {/* Left Column (md+): Video + Card */}
         <div className="h-full w-full sm:w-2/3">
           <div ref={subtitleSettingsRef}>
             <SubtitleSettings />
@@ -110,6 +153,25 @@ function TranscriptComponent() {
               Language Reactor
             </a>
           </div>
+        </div>
+                {/* Right Column (md+): Subtitles */}
+                <div
+          className="max-sm:hidden sticky top-0 max-md:border-l-0 max-md:rounded-none max-md:shadow-none md:top-2 xl:top-4 w-full max-w-xs lg:max-w-sm p-0 overflow-hidden"
+          style={{
+            maxHeight: width < 1080 ? "100dvh" : "calc(100dvh - 68px)",
+          }}
+        >
+          <SubtitleTabs setPlaying={setPlaying} getCurrentTime={getCurrentTime} onTimestampClick={onTimestampClick} />
+        </div>
+
+        {/* Mobile-only: Subtitle Panel */}
+        <div
+          className="sm:hidden pl-3 overflow-hidden space-y-2"
+          style={{
+            height: "calc(100vh - 56.25vw + 4px)", // 16:9 video aspect ratio: 9/16 = 0.5625
+          }}
+        >
+          <SubtitleTabs setPlaying={setPlaying} getCurrentTime={getCurrentTime} onTimestampClick={onTimestampClick} />
         </div>
       </div>{" "}
     </div>
